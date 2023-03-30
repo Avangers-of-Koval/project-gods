1bf8361b2b35281d6e911101cf285b420c17145f
Andrii Olkhovych <the.def4alt@gmail.com>
2023-03-30 22:43:19 +0200

    Replace circle with a texture of a player

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,37 @@
 #include "raylib.h"
 
+const int SCREEN_WIDTH = 800;
+const int SCREEN_HEIGHT = 450;
+const float PLAYER_SIZE = 64.0f;
+
 int main()
 {
-  // Initialization
-  //--------------------------------------------------------------------------------------
-  const int screenWidth = 800;
-  const int screenHeight = 450;
+  InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Project Gods");
 
-  InitWindow(screenWidth, screenHeight, "Project Gods");
-
-  Vector2 ballPosition = { static_cast<float>(screenWidth) / 2.0f, static_cast<float>(screenHeight) / 2.0f };
+  Vector2 ballPosition = { -static_cast<float>(SCREEN_WIDTH) / 2.0f + PLAYER_SIZE / 2.0f, -static_cast<float>(SCREEN_HEIGHT) / 2.0f + PLAYER_SIZE / 2.0f };
 
   SetTargetFPS(60);
 
-  while (!WindowShouldClose())    // Detect window close button or ESC key
+  auto playerTexture = LoadTexture("assets/player.png");
+
+  Rectangle source = {0, 0, 16.0f, 16.0f};
+  Rectangle dest = {0, 0, PLAYER_SIZE, PLAYER_SIZE};
+
+  while (!WindowShouldClose())
   {
-    if (IsKeyDown(KEY_RIGHT)) ballPosition.x += 2.0f;
-    if (IsKeyDown(KEY_LEFT)) ballPosition.x -= 2.0f;
-    if (IsKeyDown(KEY_UP)) ballPosition.y -= 2.0f;
-    if (IsKeyDown(KEY_DOWN)) ballPosition.y += 2.0f;
+    if (IsKeyDown(KEY_RIGHT)) ballPosition.x -= 2.0f;
+    if (IsKeyDown(KEY_LEFT)) ballPosition.x += 2.0f;
+    if (IsKeyDown(KEY_UP)) ballPosition.y += 2.0f;
+    if (IsKeyDown(KEY_DOWN)) ballPosition.y -= 2.0f;
 
     BeginDrawing();
 
     ClearBackground(RAYWHITE);
 
-    DrawText("move the ball with arrow keys", 10, 10, 20, DARKGRAY);
+    DrawTexturePro(playerTexture, source, dest, ballPosition, 0.0f, WHITE);
 
-    DrawCircleV(ballPosition, 50, MAROON);
+    DrawText("move the player with arrow keys", 10, 10, 20, DARKGRAY);
+
 
     EndDrawing();
   }
