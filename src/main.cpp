5a1f922de5342e8ccf79d27d47e6972a0dd90d1e
Andrii Olkhovych <the.def4alt@gmail.com>
2023-03-30 22:04:59 +0200

    Add circle which can be moved with arrow keys

--- src/main.cpp
+++ src/main.cpp
@@ -7,35 +7,31 @@ int main()
   const int screenWidth = 800;
   const int screenHeight = 450;
 
-  InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+  InitWindow(screenWidth, screenHeight, "Project Gods");
 
-  SetTargetFPS(60);// Set our game to run at 60 frames-per-second
-  //--------------------------------------------------------------------------------------
+  Vector2 ballPosition = { static_cast<float>(screenWidth) / 2.0f, static_cast<float>(screenHeight) / 2.0f };
 
-  // Main game loop
-  while (!WindowShouldClose())// Detect window close button or ESC key
+  SetTargetFPS(60);
+
+  while (!WindowShouldClose())    // Detect window close button or ESC key
   {
-    // Update
-    //----------------------------------------------------------------------------------
-    // TODO: Update your variables here
-    //----------------------------------------------------------------------------------
+    if (IsKeyDown(KEY_RIGHT)) ballPosition.x += 2.0f;
+    if (IsKeyDown(KEY_LEFT)) ballPosition.x -= 2.0f;
+    if (IsKeyDown(KEY_UP)) ballPosition.y -= 2.0f;
+    if (IsKeyDown(KEY_DOWN)) ballPosition.y += 2.0f;
 
-    // Draw
-    //----------------------------------------------------------------------------------
     BeginDrawing();
 
     ClearBackground(RAYWHITE);
 
-    DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+    DrawText("move the ball with arrow keys", 10, 10, 20, DARKGRAY);
+
+    DrawCircleV(ballPosition, 50, MAROON);
 
     EndDrawing();
-    //----------------------------------------------------------------------------------
   }
 
-  // De-Initialization
-  //--------------------------------------------------------------------------------------
-  CloseWindow();// Close window and OpenGL context
-  //--------------------------------------------------------------------------------------
+  CloseWindow();
 
   return 0;
 }
